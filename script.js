f498d22645d8523e298df5ef0f2de3e8ab931ddc
Sahil <singhsahil.8340@gmail.com>
2023-09-20 11:02:51 +0530

    update

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-function showTime(){
-  var d =new Date();
+function showTime() {
+  var d = new Date();
   var h = d.getHours();
   var m = d.getMinutes();
-  var s =d.getSeconds();
-var session = "AM";
+  var s = d.getSeconds();
+  var session = "AM";
 
-if(h > 12){
-  h = h-12;
-}
+  if (h > 12) {
+    h = h - 12;
+  }
 
-if (h>=12){
-  session = "PM";
-}
+  if (h >= 12) {
+    session = "PM";
+  }
 
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
 
-
-h = h < 10 ? "0"+h : h;
-m = m < 10 ? "0"+m : m;
-s = s < 10 ? "0"+s : s;
-
-  var time = h +  ":" + m +  ":" +   s +   session;
+  var time = h + ":" + m + ":" + s + session;
   document.getElementsByTagName('h1')[0].innerHTML = time;
-  setTimeout(showTime,1000);
+  setTimeout(showTime, 1000);
 
 } 
